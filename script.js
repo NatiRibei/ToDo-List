09eed4a56c20d689812094603d12e788fa976c5c
NatiRibei <natiolribeiro@gmail.com>
2022-11-25 20:24:41 -0300

    1.3

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ const addTaskButton = document.querySelector(".new-task-button");
 
 const validadeInput = ( ) => inputElement.value.trim().length > 0;
 
+const handleAddTask = () => {
+    const inputIsValid = validadeInput();
+
+    if (!inputIsValid) {
+        return inputElement.classList.add("error");
+    }
+};
+
 addTaskButton.addEventListener("click", () => validadeInput());
  
