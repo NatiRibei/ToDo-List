16fef857ac5b3348975e5ada64a29bbd089cbecf
NatiRibei <natiolribeiro@gmail.com>
2022-12-01 18:28:36 -0300

    01/12

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
+const tasksContainer = document.querySelector(".tasks-container");
 
 const validadeInput = ( ) => inputElement.value.trim().length > 0;
 
@@ -15,11 +16,54 @@ const handleAddTask = () => {
     taskItemContainer.classList.add("task-item")
 
     const taskContent = document.createElement("p");
-
     taskContent.innerText = inputElement.value;
 
-};
+    //função que quando clicar na tarefa, ele vai estilizar a tarefa para marcar como completa
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("fa-solid");
+    deleteItem.classList.add("fa-trash");
     
+    //função que quando clicar no botão delete, ele vai excluir a tarefa
+    deleteItem.addEventListener("click", () => handleDeleteClick(taskItemContainer, taskContent));
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+
+};
+// verifica em qual item filho foi clicado, se encontra aplica o estilo da classe completed
+const handleClick = (taskContent) => {
+    const tasks = tasksContainer.childNodes;
+
+    for (const task of tasks) {
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+        
+        if (currentTaskIsBeingClicked) {
+            task.firstChild.classList.toggle("completed");
+        }
+    }
+}
+
+// verifica em qual item filho foi clicado, se encontra remove a tarefa
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+    const tasks = tasksContainer.childNodes;
+
+    for (const task of tasks) {
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+        
+        if (currentTaskIsBeingClicked) {
+            taskItemContainer.remove();
+        }
+    }
+}
+
+
+
 const handleInputChange = () => {
     const inputIsValid = validadeInput ();
 
